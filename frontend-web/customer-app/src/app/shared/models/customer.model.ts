689d597d2050ebcc340c08f4e7c262fc522f8940
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-09-19 19:08:56 +0200

    Added customer model class

---
+++ frontend-web/customer-app/src/app/shared/models/customer.model.ts
@@ -0,0 +1,22 @@
+export class Customer {
+  name: string;
+  email: string;
+  city: string;
+  address: string;
+  country: string;
+  vat: number;
+  isLoyal: boolean;
+  avatar: string;
+  id?: number;
+
+  constructor(name: string, email: string, city: string, address: string, country: string, vat: number, avatar: string = 'assets/avatar.png'){
+    this.name = name;
+    this.email = email;
+    this.city = city;
+    this.address = address;
+    this.country = country;
+    this.vat = vat;
+    this.isLoyal = false;
+    this.avatar = avatar;
+  }
+}
