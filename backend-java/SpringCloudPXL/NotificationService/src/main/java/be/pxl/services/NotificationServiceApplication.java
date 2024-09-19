b9fcff72391d3116eef5e4af510f2f7a72cdf2d0
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-09-19 21:04:03 +0200

    Made a docker compose file that spins up 3 mysql databases, each service that needs to connect can connect to it's own database

--- backend-java/SpringCloudPXL/NotificationService/src/main/java/be/pxl/services/NotificationServiceApplication.java
+++ backend-java/SpringCloudPXL/NotificationService/src/main/java/be/pxl/services/NotificationServiceApplication.java
@@ -2,12 +2,13 @@ package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Notification Service Main application
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class NotificationServiceApplication
 {
     public static void main( String[] args )
