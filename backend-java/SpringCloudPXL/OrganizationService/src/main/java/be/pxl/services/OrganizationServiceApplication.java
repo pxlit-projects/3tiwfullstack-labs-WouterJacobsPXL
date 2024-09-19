207173de8330b2a57200291429e860eccc597b89
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-09-19 19:53:23 +0200

    Added the organization service module

---
+++ backend-java/SpringCloudPXL/OrganizationService/src/main/java/be/pxl/services/OrganizationServiceApplication.java
@@ -0,0 +1,17 @@
+package be.pxl.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Organization Service Main application
+ */
+
+@SpringBootApplication
+public class OrganizationServiceApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(OrganizationServiceApplication.class, args);
+    }
+}
