4f8761ce4517262677d3810d4507fcba9fe2cf64
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-09-19 19:10:06 +0200

    Customers logged to console, lab 2 finished

--- frontend-web/customer-app/src/app/app.component.ts
+++ frontend-web/customer-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Customer} from "./shared/models/customer.model";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'customer-app';
+
+  constructor() {
+    const c1 = new Customer('Dries Swinnen', 'dries@d-ries.be','Pelt','mystreet','Belgium',21);
+    const c2 = new Customer('John Doe', 'john@doe.com','New York','5th Avenue','USA',6, 'assets/john.png');
+    c2.isLoyal = true;
+
+    console.log(c1);
+    console.log(c2);
+  }
 }
