aa4c1878a3c64c1d2a85daf8b28f0047e77e3e82
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-10-03 15:44:56 +0200

    Lab 4 done

--- frontend-web/customer-app/src/app/core/customers/customer-item/customer-item.component.ts
+++ frontend-web/customer-app/src/app/core/customers/customer-item/customer-item.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Customer} from "../../../shared/models/customer.model";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-customer-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './customer-item.component.html',
   styleUrl: './customer-item.component.css'
 })
-export class CustomerItemComponent {
 
+export class CustomerItemComponent {
+  @Input() customer!: Customer;
+
+  getDetails(): void{
+    console.log(this.customer);
+  }
 }
--- frontend-web/customer-app/src/app/core/customers/customer-list/customer-list.component.ts
+++ frontend-web/customer-app/src/app/core/customers/customer-list/customer-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CustomerItemComponent} from "../customer-item/customer-item.component";
 import {FilterComponent} from "../filter/filter.component";
+import {Customer} from "../../../shared/models/customer.model";
 
 @Component({
   selector: 'app-customer-list',
@@ -12,6 +13,17 @@ import {FilterComponent} from "../filter/filter.component";
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
 })
-export class CustomerListComponent {
 
+export class CustomerListComponent implements OnInit {
+  customers!: Customer[];
+
+  ngOnInit(): void {
+    this.customers = [
+      new Customer('Dries Swinnen', 'dries@pxl.be', 'Pelt', 'mystreet', 'Belgium', 21),
+      new Customer('John Doe', 'john@doe.be', 'New York', '5th Avenue', 'USA', 6),
+      new Customer('Jane Doe', 'jane@doe.be', 'Los Angeles', 'Sunset Boulevard', 'USA', 6)
+    ];
+
+    this.customers[1].isLoyal = true;
+  }
 }
