2991915a783ad63b163ac70740b95f73fdd2247f
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-10-03 16:14:22 +0200

    Adding tailwind + logo component

--- frontend-web/Chapter3/chapter3-exercise1/src/app/app.component.ts
+++ frontend-web/Chapter3/chapter3-exercise1/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LogoComponent} from "./logo/logo.component";
+import {NewsletterComponent} from "./newsletter/newsletter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogoComponent, NewsletterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
