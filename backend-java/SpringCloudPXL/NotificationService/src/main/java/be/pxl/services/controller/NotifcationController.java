0cec448c5c092784835809e37b6fc452a803ac82
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-10-05 01:06:11 +0200

    Made sendMessage endpoint for the notificationService

--- backend-java/SpringCloudPXL/NotificationService/src/main/java/be/pxl/services/controller/NotifcationController.java
+++ backend-java/SpringCloudPXL/NotificationService/src/main/java/be/pxl/services/controller/NotifcationController.java
@@ -1,7 +1,28 @@
 package be.pxl.services.controller;
 
+import be.pxl.services.domain.Notification;
+import be.pxl.services.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/notification")
 public class NotifcationController {
+    @Autowired
+    private NotificationService notificationService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody Notification notification){
+        notificationService.sendMessage(notification);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 }
--- backend-java/SpringCloudPXL/NotificationService/src/main/java/be/pxl/services/service/NotificationService.java
+++ backend-java/SpringCloudPXL/NotificationService/src/main/java/be/pxl/services/service/NotificationService.java
@@ -1,7 +1,14 @@
 package be.pxl.services.service;
 
+import be.pxl.services.domain.Notification;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
+    public void sendMessage(Notification notification) {
+        System.out.println("Receiving information");
+        System.out.printf("Sending ... {%s}%n", notification.getMessage());
+        System.out.printf("To ... {%s}%n", notification.getTo());
+
+    }
 }
